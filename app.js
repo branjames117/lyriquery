69dbd832668358d7de3776ddc166e75405d13bc5
branjames117 <branjames117@gmail.com>
2021-04-27 19:37:26 -0500

    prepared to deploy

--- app.js
+++ app.js
@@ -202,6 +202,6 @@ app.post('/submitupdate', (req, res) => {
 });
 
 // SERVER LISTEN ON PORT 3000 /////////////////////////////////////////////////////////
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, function() {
+  console.log(`Server started on port ${process.env.PORT}`);
 });
